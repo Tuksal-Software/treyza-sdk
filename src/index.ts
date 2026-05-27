e5bf4b995fc2a5cf4a350f6998035c652fee4105
Ibrahim Tuksal <tuksalibrahim@gmail.com>
2026-05-27 11:01:46 +0300

    feat(sdk): add root entry exports
    Aggregates public SDK surface at the package root: types, cn,
    TreyzaSDKProvider, useTreyza and all hooks for direct
    import from @treyza/sdk.

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export type * from "./types";
+export { cn } from "./lib/utils";
+export { TreyzaSDKProvider, useTreyza } from "./provider";
+export {
+  useConfig,
+  useTheme,
+  useStore,
+  useProducts,
+  useProduct,
+  useCategories,
+  useCart,
+  useCustomer,
+  useWishlist,
+  useSearch,
+  useInView,
+  useAnimation,
+  usePagination,
+  useFilter,
+} from "./hooks";
